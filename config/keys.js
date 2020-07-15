92f221309914868b11ddf3b584f84ed535782fa9
Mohsin Khan Shuvo <mkshuvo@hotmail.com>
2020-07-15 23:35:44 +0600

    database cloud url added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-url2 = "mongodb://localhost:27017/advising";
+url2 = "mongodb+srv://mkshuvo:iamshuvo123@cluster0-zht09.mongodb.net/advising?retryWrites=true&w=majority";
 
 module.exports = {
   mongoURI: url2,
